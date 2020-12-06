4ad2d7f0849e737c248418cabe085857f78791a9
Tjaša Trafela <tjasa.trafela@student.fmf.uni-lj.si>
2020-12-06 17:02:47 +0100

    grafi zivine

--- uvoz/Uvoz1.r
+++ uvoz/Uvoz1.r
@@ -62,14 +62,28 @@ uvoz.zivina$leto <- as.integer(uvoz.zivina$leto)
 #kolko je bilo zivine v teh letih v povprecju po regijah
 povprecje.zivine.regije <- uvoz.zivina %>% group_by(vrsta.zivine, regija) %>% 
   summarise(povprecje = mean(stevilo.zivali, na.rm = TRUE))
+#graf za najbolj pogostih po regijah
+graf.zivina.regije <- povprecje.zivine.regije %>% ggplot(aes(x = regija, y = povprecje)) +
+  geom_point() + facet_wrap(~vrsta.zivine, ncol = 6) + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+graf.zivina.regije
+
 
 #kolko je povprečno zivine v Sloveniji v teh letih
 povprecje.zivine.slovenija <- povprecje.zivine.regije %>% group_by(vrsta.zivine) %>% 
   summarise(povprecje = mean(povprecje, na.rm = TRUE))
+#diagram
+graf.povprecje.zivine.slovenija <- ggplot(aes(x = vrsta.zivine, y = povprecje, group=1), 
+                      data = povprecje.zivine.slovenija) + geom_col() + ggtitle("Povprečje živine")
+graf.povprecje.zivine.slovenija
 
 #koliko je v posamezne letu zivine povprečno
 povprecje.zivine.leta <- uvoz.zivina %>% group_by(vrsta.zivine, leto) %>% 
   summarise(povprecje = mean(stevilo.zivali, na.rm = TRUE))
+#graf kako skozi leta spreminjala količina zivine v sloveniji
+graf.zivina.leta <- povprecje.zivine.leta %>% ggplot(aes(x = leto, y = povprecje)) +
+  geom_step() + facet_wrap(~vrsta.zivine, ncol = 6) + scale_x_continuous(breaks = seq(2003, 2016, 2)) +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1))
+graf.zivina.leta
 
 #kje je najvec zivine skozi leta
 najvec.zivine.regije <- povprecje.zivine.regije %>% group_by(regija) %>% 
