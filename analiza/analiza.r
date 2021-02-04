ecd61ec2b030f63c00ad1f5a3b79f9013c487920
Tjaša Trafela <tjasa.trafela@student.fmf.uni-lj.si>
2021-02-04 21:22:30 +0100

    popravek v analiza.r

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -15,7 +15,7 @@
 #Kako se količina pridelkov skupno skozi leta spreminja
 g1 <- ggplot(pridelki.leta, aes(x=leto, y=povprecje)) + geom_point(color='dark blue')
 lin1 <- lm(data = pridelki.leta, povprecje ~ leto)
-graf1 = g + geom_smooth(method='lm',fullrange=TRUE,color='black', formula = y~x)
+graf1 = g1 + geom_smooth(method='lm',fullrange=TRUE,color='black', formula = y~x)
 nova1 <- data.frame(leto=seq(2010, 2025, 1))
 napoved1 <- mutate(nova1, povprecje=predict(lin1, nova1))
 graf_napoved1 <- graf1 + geom_point(data=napoved1, aes(x=leto, y=povprecje), color='red') +
